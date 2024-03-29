a17e974ec8f7893be758ab366c18771a4e8a8ccd
MatiXdou <163367319+MatiXdou@users.noreply.github.com>
2024-03-29 20:20:32 -0300

    jquery meh

--- js/jquery.js
+++ js/jquery.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-    $('form').submit(function(){
+    $('form').submit(function(e) {
         e.preventDefault();
 
         //validaciones
@@ -8,7 +8,17 @@ $(document).ready(function() {
             alert('Lo sentimos, el nombre jose no esta permitido');
             return;
         }
+        
+
+        //validar edad
+        var edad = parseInt($('#edad').val());
+        if(edad <= 17 || edad > 65 || isNaN(edad)) {
+            alert('Por favor, ingresa una edad entre 18 y 65.');
+            return;
+        }
+
         //si tienes todas las validaciones exitosas, puedes enviar el formulario
         this.submit();
+
     });
 });
